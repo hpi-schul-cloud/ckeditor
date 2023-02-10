7065d501770af0151df14f4063cb2d7978a9a999
Christian Darsow <104998795+christian-darsow@users.noreply.github.com>
2023-02-10 14:18:56 +0100

    BC-3094 - Fix translations (#7)
    * Fix translations
    
    * remove specific npm version

--- src/locales/en.js
+++ src/locales/en.js
@@ -1,7 +1,10 @@
 import { add } from "@ckeditor/ckeditor5-utils/src/translation-service.js";
 
 const translations = {
+    "Equation preview": "Preview",
+    "Insert equation in TeX format.": "Insert equation in TeX format.",
     "Display mode": "Line break",
+    "Insert math": "Formula",
 };
 
 export default function() {
