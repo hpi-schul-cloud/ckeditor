919e9e4bd94be972e0f752fd6e707e64c84a284a
Christian Darsow <christian.darsow@dataport.de>
2023-02-10 14:15:52 +0100

    Make language keys consistent

--- src/locales/en.js
+++ src/locales/en.js
@@ -2,6 +2,7 @@ import { add } from "@ckeditor/ckeditor5-utils/src/translation-service.js";
 
 const translations = {
     "Equation preview": "Preview",
+    "Insert equation in TeX format.": "Insert equation in TeX format.",
     "Display mode": "Line break",
     "Insert math": "Formula",
 };
